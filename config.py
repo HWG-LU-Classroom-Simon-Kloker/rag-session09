5a8ff783d1d234d647b37347a454d2205c829a50
simon-kloker <simon.kloker@mail.de>
2026-07-22 11:23:16 +0200

    Add configuration settings for models and parameters

---
+++ config.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+FAISS_INDEX = Path("./faiss_index")
+EMBED_MODEL = "embed-english-v3.0"
+RERANK_MODEL = "rerank-english-v3.0"
+GENERATION_MODEL = "command-r-plus-08-2024"
+RETRIEVAL_K = 10
+RERANK_TOP_N = 3
